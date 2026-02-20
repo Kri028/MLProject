9e28011d7a99a090e46e3d547327cfddef52e4f8
Krishna Kumar Mishra <krishnamishra028@gmail.com>
2026-02-20 16:57:53 +0000

    Edit predict_pipeline.py

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -8,25 +8,25 @@ class PredictPipeline:
     def __init__(self):
         pass
 
-class PredictPipeline:
-    def __init__(self):
-        pass
     def predict(self,features):
         try:
-            model_path='artifacts/model.pkl'
-            preprocessor_path='artifacts/preprocessor.pkl'
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            print("Before Loading")
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
+            print("After Loading")
             data_scaled=preprocessor.transform(features)
             preds=model.predict(data_scaled)
             return preds
-        except Exception as e:
-            raise CustomException(e, sys)
-
         
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
 
 class CustomData:
-    def __init__( self,
+    def __init__(  self,
         gender: str,
         race_ethnicity: str,
         parental_level_of_education,
@@ -44,12 +44,12 @@ class CustomData:
         self.lunch = lunch
 
         self.test_preparation_course = test_preparation_course
+
         self.reading_score = reading_score
+
         self.writing_score = writing_score
 
-
     def get_data_as_data_frame(self):
-        
         try:
             custom_data_input_dict = {
                 "gender": [self.gender],
@@ -59,13 +59,11 @@ class CustomData:
                 "test_preparation_course": [self.test_preparation_course],
                 "reading_score": [self.reading_score],
                 "writing_score": [self.writing_score],
-
             }
 
             return pd.DataFrame(custom_data_input_dict)
-        
+
         except Exception as e:
             raise CustomException(e, sys)
-
-
-    
+        
+        ## Make Some Changes
